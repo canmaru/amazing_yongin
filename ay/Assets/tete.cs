1c3a273c9df14241873d7f839916f32ce6d98859
canmaru <jeph.yi@canmaru.com>
2016-12-26 17:33:01 +0900

    Revert "Revert "몬스터 및 패턴 유저정보 다운로드""
    This reverts commit 7de6346e9a01997d7720a0f626bc73484ba4e933.

---
+++ ay/Assets/tete.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tete : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        data_manager.Instance.init_data();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
